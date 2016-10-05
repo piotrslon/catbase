a1a84384d1730aedb144868d608b541e6bea7ab3
Piotr Slon <peslonx@gmail.com>
2016-10-05 13:07:09 +0200

    create interface

---
+++ src/main/java/com/testcatname/catbase/Interface.java
@@ -0,0 +1,31 @@
+package com.testcatname.catbase;
+
+import java.util.Scanner;
+
+import com.testcatname.catsbase.model.Cat;
+
+public class Interface  {
+
+	static Scanner sc = new Scanner(System.in); 
+	
+	public static void main(String[] args) {
+		
+		Cat cat = new Cat();
+		
+		System.out.println("Give cats name: ");
+		String name = getUserInput();
+		System.out.println("Give cat guardians name");
+		String guardianName = getUserInput();
+		
+		cat.setName(name);
+		cat.setGuardianName(guardianName);
+		
+		
+	}
+
+	public static String getUserInput() {
+		return sc.nextLine();
+	}
+	
+
+}
--- src/main/java/com/testcatname/catsbase/model/Cat.java
+++ src/main/java/com/testcatname/catsbase/model/Cat.java
@@ -5,15 +5,47 @@ import java.text.SimpleDateFormat;
 
 public class Cat {
 	
-	private String name = "Mila";
-	private Date today = new Date(0);
+	private String name;
+	private Date today;
 	SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm");
 	//System.out.println(sdf.format(date))
-	private double weight = 3.14;
-	private String guardianName = "Angleo";
+	private double weight;
+	private String guardianName;
 	
 	public String tellYourName(){
 		return "This cat names" +name+ "he born in" +(sdf.format(today))+"his weight is"+weight+"and his guargian name is"+guardianName;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getToday() {
+		return today;
+	}
+
+	public void setToday(Date today) {
+		this.today = today;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	public String getGuardianName() {
+		return guardianName;
+	}
+
+	public void setGuardianName(String guardianName) {
+		this.guardianName = guardianName;
 	}
 
 }
