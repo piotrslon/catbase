df7a661ca3b218a8f05fc44f527e82ef4a224b3d
Piotr Slon <peslonx@gmail.com>
2016-10-06 13:20:35 +0200

    regEx

--- src/main/java/com/testcatname/catbase/Interface.java
+++ src/main/java/com/testcatname/catbase/Interface.java
@@ -3,6 +3,7 @@ package com.testcatname.catbase;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 import com.testcatname.catsbase.model.Cat;
 
@@ -14,27 +15,37 @@ public class Interface  {
 		
 		Cat cat = new Cat();
 		SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
-		
-		
-		System.out.println("Give cats name: ");
-		cat.setName(getUserInput());
+		Pattern dataRexgex = Pattern.compile("[0-3]?[0-9].[0-1]?[0-9].([0-9]){4}");
+		String bornDateSc;
+		String catNameSc;
+		boolean lit = true;
 		
 		do {
-		    try {
-				System.out.println("Give cat born date");
-				cat.setBirthDate(sdf.parse(getUserInput()));
-			} catch (ParseException pe) {
-				System.out.println("Smf wrong with dataformat, eg. 'dd.MM.yyyy'");
+			System.out.println("Give cats name: ");
+			catNameSc = getUserInput();
+			lit = Pattern.matches("[a-zA-Z]+[0-9]*", catNameSc);
+			if (lit) cat.setName(catNameSc);
+		} while (!lit);
+		
+		do {
+			System.out.println("Give cat born date in format 'dd.MM.yyyy'");
+			bornDateSc = getUserInput();
+			if(dataRexgex.matcher(bornDateSc).matches()) {
+				try {
+					cat.setBirthDate(sdf.parse(bornDateSc));
+				} catch (ParseException pe) {
+					System.out.println("Smf wrong with dataformat! example data: '01.01.2000'");
+				}
 			}
 		} while (cat.getBirthDate() == null);
-		
+			
 		do {
 			try {
 				System.out.println("Give cat weight: eg. '4.56'");
 				cat.setWeight(Float.valueOf(getUserInput()));
 			} catch (NumberFormatException nfe) {
 				//nfe.printStackTrace();
-				System.out.println("Smf wrong with weightformat, eg. '4.56'");
+				System.out.println("Smf wrong with weightformat '4.56'");
 			} 
 		} while (cat.getWeight() == null);
 		
