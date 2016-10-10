40c1b5a9cf4e9bb55665367d035fc234d4df49f6
Piotr Slon <peslonx@gmail.com>
2016-10-10 13:55:10 +0200

    move code to methods

--- src/main/java/com/testcatname/catbase/Interface.java
+++ src/main/java/com/testcatname/catbase/Interface.java
@@ -11,19 +11,12 @@ import com.testcatname.catsbase.model.Cat;
 
 public class Interface  {
 
-	static Scanner sc = new Scanner(System.in); 
+	static Scanner sc = new Scanner(System.in);
+	
+	static CatDAO catDAO = new CatDAO();
 	
 	public static void main(String[] args) {
 		
-		Cat cat = new Cat();
-		//Alpha cAlpha = new Alpha();  
-		CatDAO catDAO = new CatDAO();
-		SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
-		Pattern dataRexgex = Pattern.compile("[0-3]?[0-9].[0-1]?[0-9].([0-9]){4}");
-		String bornDateSc;
-		String catNameSc;
-		Integer catsIndex = 0;
-		boolean lit = true;
 		String choice = "";
 		
 		do {
@@ -33,66 +26,9 @@ public class Interface  {
 			choice = getUserInput();
 			do {
 				if (choice.equalsIgnoreCase("1")) {
-					//add cat
-					do {
-						System.out.println("Give cats name: ");
-						catNameSc = getUserInput();
-						lit = Pattern.matches("[a-zA-Z]+[0-9]*", catNameSc);
-						if (lit)
-							cat.setName(catNameSc);
-					} while (!lit);
-
-					do {
-						System.out.println("Give cat born date in format 'dd.MM.yyyy'");
-						bornDateSc = getUserInput();
-						if (dataRexgex.matcher(bornDateSc).matches()) {
-							try {
-								cat.setBirthDate(sdf.parse(bornDateSc));
-							} catch (ParseException pe) {
-								System.out.println("Smf wrong with dataformat! example data: '01.01.2000'");
-							}
-						}
-					} while (cat.getBirthDate() == null);
-
-					do {
-						try {
-							System.out.println("Give cat weight: eg. '4.56'");
-							cat.setWeight(Float.valueOf(getUserInput()));
-						} catch (NumberFormatException nfe) {
-							//nfe.printStackTrace();
-							System.out.println("Smf wrong with weightformat '4.56'");
-						}
-					} while (cat.getWeight() == null);
-
-					System.out.println("Give cat guardians name");
-					cat.setGuardianName(getUserInput());
-					
-					catDAO.addCat(cat);
-
+					addCat();
 				} else if (choice.equalsIgnoreCase("2")) {
-					//list cat
-					List<Cat> cats = catDAO.getCats();
-					if (cats.size() == 0) {
-						System.out.println("Cats base is empty");
-					} else {
-						System.out.println("Choose your cat: ");
-						for (int i=0;i<cats.size();i++){
-							System.out.println(i+1+" - "+cats.get(i).getName());
-						}
-						
-						do {
-							try {
-								catsIndex = Integer.valueOf(getUserInput())-1;
-								//show cat
-								System.out.println(cats.get(catsIndex).tellYourName());
-								continue;
-							} catch (NumberFormatException nfe) {
-								System.out.println("Only numbers");
-							} catch (IndexOutOfBoundsException iob) {
-								System.out.println("Try again");
-							} 
-						} while (catsIndex != 0);
-					}
+					showCat();
 				} else if (choice.equalsIgnoreCase("x")) {
 					break;
 				} else {
@@ -105,6 +41,78 @@ public class Interface  {
 
 	public static String getUserInput() {
 		return sc.nextLine().trim();
+	}
+	private static void addCat() {
+		//add cat
+		Cat cat = new Cat();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+		Pattern dataRexgex = Pattern.compile("[0-3]?[0-9].[0-1]?[0-9].([0-9]){4}");
+		String bornDateSc;
+		String catNameSc;
+		boolean lit = true;
+		
+		do {
+			System.out.println("Give cats name: ");
+			catNameSc = getUserInput();
+			lit = Pattern.matches("[a-zA-Z]+[0-9]*", catNameSc);
+			if (lit)
+				cat.setName(catNameSc);
+		} while (!lit);
+
+		do {
+			System.out.println("Give cat born date in format 'dd.MM.yyyy'");
+			bornDateSc = getUserInput();
+			if (dataRexgex.matcher(bornDateSc).matches()) {
+				try {
+					cat.setBirthDate(sdf.parse(bornDateSc));
+				} catch (ParseException pe) {
+					System.out.println("Smf wrong with dataformat! example data: '01.01.2000'");
+				}
+			}
+		} while (cat.getBirthDate() == null);
+
+		do {
+			try {
+				System.out.println("Give cat weight: eg. '4.56'");
+				cat.setWeight(Float.valueOf(getUserInput()));
+			} catch (NumberFormatException nfe) {
+				//nfe.printStackTrace();
+				System.out.println("Smf wrong with weightformat '4.56'");
+			}
+		} while (cat.getWeight() == null);
+
+		System.out.println("Give cat guardians name");
+		cat.setGuardianName(getUserInput());
+		
+		catDAO.addCat(cat);
+	}
+	
+	private static void showCat(){
+		//list cat
+		List<Cat> cats = catDAO.getCats();
+		Integer catsIndex = 0;
+		
+		if (cats.size() == 0) {
+			System.out.println("Cats base is empty");
+		} else {
+			System.out.println("Choose your cat: ");
+			for (int i=0;i<cats.size();i++){
+				System.out.println(i+1+" - "+cats.get(i).getName());
+			}
+			
+			do {
+				try {
+					catsIndex = Integer.valueOf(getUserInput())-1;
+					//show cat
+					System.out.println(cats.get(catsIndex).tellYourName());
+					continue;
+				} catch (NumberFormatException nfe) {
+					System.out.println("Only numbers");
+				} catch (IndexOutOfBoundsException iob) {
+					System.out.println("Try again");
+				} 
+			} while (catsIndex != 0);
+		}
 	}
 	
 }
